931c14ee8141bd73c573c7e0ecfc865da6525a3e
Fayddelight <veitebbers@hotmail.de>
2020-09-23 22:42:30 +0300

    app component structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
@@ -7,13 +7,42 @@ import TodoList from "./components/TodoList";
 //comment
 
 export default class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "wake up" },
+      { id: 2, title: "make breakfast" },
+    ],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {};
+  handleSubmit = (e) => {};
+  clearList = () => {};
+  handleDelete = (id) => {};
+  handleEdit = (id) => {};
+
   render() {
     return (
       <div>
         <div className="container">
           <div className="row">
-            <TodoInput></TodoInput>
-            <TodoList></TodoList>
+            <div className="col-10 mx-auto col-md-8 mt-5">
+              <h3 className="text-capitalize text-center">TodoInput</h3>
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.state.handleChange}
+                handleSubmit={this.state.handleSubmit}
+                editItem={this.state.editItem}
+              ></TodoInput>
+              <TodoList
+                items={this.state.items}
+                clearList={this.state.clearList}
+                handleDelete={this.state.handleDelete}
+                handleEdit={this.state.handleEdit}
+              ></TodoList>
+            </div>
           </div>
         </div>
       </div>
